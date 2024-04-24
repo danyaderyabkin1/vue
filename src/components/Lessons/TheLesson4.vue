a0b3f8ebc1fc248c202e1c0dd2e72303c0110f76
danya <eddanya@gmail.com>
2024-04-24 17:37:25 +0300

    dev vue 4-task done

--- src/components/Lessons/TheLesson4.vue
+++ src/components/Lessons/TheLesson4.vue
@@ -1,12 +1,32 @@
 <script setup>
-import {ref} from 'vue';
+import {computed, ref} from 'vue';
 import ProductItem from "@/components/ProductsList/ProductItem.vue";
+import axios from "axios";
+import {API_BASE_URL} from "@/config.js";
 const products = ref([]);
 
+const page = 1;
+const perPage = 4;
 
-fetch('https://vue-study.skillbox.cc/api/products')
-    .then(response => response.json())
-    .then(data => products.value = data.items)
+
+
+async function getProducts() {
+  return await (new Promise(resolve => setTimeout(resolve, 200)))
+      .then(() => {
+        return axios.get(`${API_BASE_URL}/api/products`, {
+          params: {
+            page: page,
+            limit: perPage,
+
+          }
+        })
+            .then(response => {
+              console.log(response.data)
+              products.value = response.data
+            })
+      })
+}
+getProducts()
 
 </script>
 
@@ -14,10 +34,28 @@ fetch('https://vue-study.skillbox.cc/api/products')
   <div class="container container3 mt-3 mb-3 flex-fill text-center p-3">
     <h2 class="text-center mb-4">Четвертая задача</h2>
       <ul class="list-unstyled d-flex flex-wrap justify-content-between">
-      <product-item v-for="product in products" :product="product"/>
+      <product-item v-for="product of products.items" :product="product"/>
       </ul>
+    <ul class="list-unstyled pagination">
+      <li style="display: none">
+        <button class="btn btn-light">&#8249;</button>
+      </li>
+        <li  v-for="page in products.pagination.pages">
+          <button @click.prevent="getProducts" >{{page}}</button>
+        </li>
+
+        <li style="display: none">
+          <button @click.prevent="page++" class="btn btn-light">&#8250;</button>
+        </li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  gap: 5px;
+}
 </style>
